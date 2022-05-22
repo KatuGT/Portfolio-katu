3b2cc87f4fe150df25ebc44614eb7e079a5425e5
JimenaKatuMartinez <93katu@gmail.com>
2022-05-22 11:19:36 -0300

    aplicados estilos para proyectos para pc

--- src/componentes/Proyectos.jsx
+++ src/componentes/Proyectos.jsx
@@ -1,14 +1,33 @@
 import styled from "styled-components";
 import TituloSecciones from "./TituloSecciones";
 import ImgRollflixMobile from "../imagenes/pr-rollflixMobile.jpg";
+import ImgRollflix from "../imagenes/pr-rollflix.jpg";
 import ImgMoviePopMobile from "../imagenes/pr-moviepopMobile.jpg";
+import ImgMoviePop from "../imagenes/pr-moviepop.jpg";
 import ImgCVMobile from "../imagenes/pr-cvMobile.jpg";
+import ImgCV from "../imagenes/pr-cv.jpg";
 
-const WrapperProyectos = styled.section``;
+const WrapperProyectos = styled.section`
+  width: 70%;
+  margin: 0 auto;
+`;
 
 const Proyecto = styled.div`
   text-align: center;
-  margin-bottom: 4em;
+  margin-bottom: 9em;
+  height: 500px;
+  @media screen and (min-width: 900px) {
+    display: grid;
+    place-items: center;
+    gap: 2em;
+    grid-template-columns: 1fr 2fr;
+    grid-template-rows: 1fr;
+  }
+`;
+
+const InfoProyecto = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 const TituloProyecto = styled.h3`
   font-weight: bold;
@@ -19,57 +38,103 @@ const TituloProyecto = styled.h3`
 
 const DescProyecto = styled.p`
   margin-top: 0.2em;
-  margin-bottom:0.4em;
+  margin-bottom: 0.4em;
+`;
+
+const LinkImagenes = styled.a`
+  position: relative;
+  height: 500px;
+`;
+
+const ImgProyectoMobile = styled.img`
+  border-radius: 1em;
+  position: relative;
+  box-shadow: 0px 0px 0 4px var(--main-clr), 20px 15px 50px rgba(0, 0, 0, 0.5);
+
+  @media screen and (min-width: 500px) {
+    display: none;
+  }
+
+  @media screen and (min-width: 900px) {
+    display: block;
+    height: 80%;
+    width: auto;
+    position: absolute;
+    top: 40%;
+  }
+
+  &.right {
+    @media screen and (min-width: 900px) {
+      right: -20%;
+    }
+  }
+
+  &.left {
+    @media screen and (min-width: 900px) {
+      left: 80%;
+    }
+  }
 `;
 
 const ImgProyecto = styled.img`
-  width: 100%;
+  height: 100%;
 `;
 
 const Proyectos = () => {
   return (
-    <section  id="proyectos">
+    <section id="proyectos">
       <TituloSecciones>Proyectos</TituloSecciones>
       <WrapperProyectos>
         <Proyecto>
-          <TituloProyecto>Rollflix</TituloProyecto>
-          <DescProyecto>
-            Clon de Netflix, fue mi proyecto final en el curso FullStack (MERN)
-            de RollingCode School.
-          </DescProyecto>
-          <a
+          <InfoProyecto>
+            <TituloProyecto>Rollflix</TituloProyecto>
+            <DescProyecto>
+              Clon de Netflix, fue mi proyecto final en el curso FullStack
+              (MERN) de RollingCode School.
+            </DescProyecto>
+          </InfoProyecto>
+          <LinkImagenes
             href="https://effortless-centaur-f31ca1.netlify.app/"
             rel="noopener noreferrer"
             target="_blank"
           >
-            <ImgProyecto src={ImgRollflixMobile} />
-          </a>
+            <ImgProyectoMobile src={ImgRollflixMobile} className="left" />
+            <ImgProyecto src={ImgRollflix} />
+          </LinkImagenes>
         </Proyecto>
         <Proyecto>
-          <TituloProyecto>MoviePop</TituloProyecto>
-          <DescProyecto>Otra página de películas, lo hice como parte de una entrevista.</DescProyecto>
-          <a
+          <LinkImagenes
             href="https://incandescent-taffy-ad42eb.netlify.app/peliculas"
             rel="noopener noreferrer"
             target="_blank"
           >
-            <ImgProyecto src={ImgMoviePopMobile} />
-          </a>
+            <ImgProyectoMobile src={ImgMoviePopMobile} className="right" />
+            <ImgProyecto src={ImgMoviePop} />
+          </LinkImagenes>
+          <InfoProyecto>
+            <TituloProyecto>MoviePop</TituloProyecto>
+            <DescProyecto>
+              Otra página de películas, lo hice como parte de una entrevista.
+            </DescProyecto>
+          </InfoProyecto>
         </Proyecto>
         <Proyecto>
-          <TituloProyecto>CV Falso</TituloProyecto>
-
-          <DescProyecto>
-            Realizado para un concurso durante mi cursado ( el cual gane &#128512; ),
-            por lo que algo de la información que verán ahí es ficticia.
-          </DescProyecto>
-          <a
+          <InfoProyecto>
+            <TituloProyecto>CV Falso</TituloProyecto>
+            <DescProyecto>
+              Realizado para un concurso durante mi cursado ( el cual gane
+              &#128512; ), por lo que algo de la información que verán ahí es
+              ficticia.
+            </DescProyecto>
+          </InfoProyecto>
+          <LinkImagenes
             href="https://vigorous-darwin-6aa540.netlify.app/"
             rel="noopener noreferrer"
             target="_blank"
           >
-            <ImgProyecto src={ImgCVMobile} />
-          </a>
+            <ImgProyectoMobile src={ImgCVMobile}  className="left" />
+            <ImgProyecto src={ImgCV} />
+          </LinkImagenes>
         </Proyecto>
       </WrapperProyectos>
     </section>
