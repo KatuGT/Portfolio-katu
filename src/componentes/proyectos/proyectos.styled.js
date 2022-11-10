1d62a05ffd213010927c9f6abe7af09488cf9d78
JimenaKatuMartinez <93katu@gmail.com>
2022-11-10 20:06:35 -0300

    arreglado estilo para mobile

--- src/componentes/proyectos/proyectos.styled.js
+++ src/componentes/proyectos/proyectos.styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const MainWrapperPruectos = styled.section`
   width: 100%;
+  margin: 0 auto;
 `;
 
 export const WrapperProyectos = styled.section`
