d8f7e02396889851a6388081242cf489ea6586e8
katu <93katu@gmail.com>
2023-09-03 21:14:12 -0300

    alineados projectos y tecnologias

--- src/componentes/tecnologias/Tecnologias.jsx
+++ src/componentes/tecnologias/Tecnologias.jsx
@@ -20,6 +20,8 @@ import {
   Logo,
   NuevoTituloSeccion,
   WrapperLogos,
+  WrapperTech,
+  WrapperTechText,
   WrapperTecnologias,
 } from "./tecnologias.styled";
 import { Titulo } from "../tituloSecciones/tituloSecciones";
@@ -34,22 +36,85 @@ const Tecnologias = () => {
       <WrapperTecnologias id="tecno">
         <Titulo>{t("toolsAndTech")}</Titulo>
         <WrapperLogos>
-          <Logo src={LogoHTML} alt="Logo HTML" />
-          <Logo src={LogoCSS} alt="Logo CSS" />
-          <Logo src={LogoJS} alt="Logo Javascript" />
-          <Logo src={LogoTS} alt="Logo Javascript" />
-          <Logo src={LogoBoots} alt="Logo Bootstrap" />
-          <Logo src={LogoTailwind} alt="Logo Tailwind css" />
-          <Logo src={LogoNext} alt="Logo Next"/>
-          <Logo src={LogoReact} alt="Logo React" />
-          <Logo src={LogoReactNative} alt="Logo React Native" />
-          <Logo src={LogoStyled} alt="Logo styled Components" />
-          <Logo src={LogoVS} alt="Logo VisualStudio Code" />
-          <Logo src={LogoFigma} alt="Logo Figma" />
-          <Logo src={LogoPS} alt="Logo Photoshop" />
-          <Logo src={LogoAI} alt="Logo Illustrator" />
-          <Logo src={LogoSASS} alt="Logo Illustrator" />
-          <Logo src={LogoRedux} alt="Logo Illustrator" />
+          <WrapperTech>
+            <Logo src={LogoHTML} alt="Logo HTML" />
+            <WrapperTechText>HTML 5</WrapperTechText>
+          </WrapperTech>
+
+          <WrapperTech>
+            <Logo src={LogoCSS} alt="Logo CSS" />
+            <WrapperTechText>CSS</WrapperTechText>
+          </WrapperTech>
+
+          <WrapperTech>
+            <Logo src={LogoJS} alt="Logo Javascript" />
+            <WrapperTechText>JavaScript</WrapperTechText>
+          </WrapperTech>
+
+          <WrapperTech>
+            <Logo src={LogoTS} alt="Logo TypeScript" />
+            <WrapperTechText>TypeScript</WrapperTechText>
+          </WrapperTech>
+
+          <WrapperTech>
+            <Logo src={LogoBoots} alt="Logo Bootstrap" />
+            <WrapperTechText>Bootstrap</WrapperTechText>
+          </WrapperTech>
+
+          <WrapperTech>
+            <Logo src={LogoTailwind} alt="Logo Tailwind css" />
+            <WrapperTechText>Tailwind</WrapperTechText>
+          </WrapperTech>
+
+          <WrapperTech>
+            <Logo src={LogoNext} alt="Logo Next" />
+            <WrapperTechText>Next JS</WrapperTechText>
+          </WrapperTech>
+
+          <WrapperTech>
+            <Logo src={LogoReact} alt="Logo React" />
+            <WrapperTechText>React JS</WrapperTechText>
+          </WrapperTech>
+
+          <WrapperTech>
+            <Logo src={LogoReactNative} alt="Logo React Native" />
+            <WrapperTechText>React Native</WrapperTechText>
+          </WrapperTech>
+
+          <WrapperTech>
+            <Logo src={LogoStyled} alt="Logo styled Components" />
+            <WrapperTechText>Styled Components</WrapperTechText>
+          </WrapperTech>
+
+          <WrapperTech>
+            <Logo src={LogoVS} alt="Logo VisualStudio Code" />
+            <WrapperTechText>VS Code</WrapperTechText>
+          </WrapperTech>
+
+          <WrapperTech>
+            <Logo src={LogoFigma} alt="Logo Figma" />
+            <WrapperTechText>Figma</WrapperTechText>
+          </WrapperTech>
+
+          <WrapperTech>
+            <Logo src={LogoPS} alt="Logo Photoshop" />
+            <WrapperTechText>Photoshop</WrapperTechText>
+          </WrapperTech>
+
+          <WrapperTech>
+            <Logo src={LogoAI} alt="Logo Illustrator" />
+            <WrapperTechText>Illustrator</WrapperTechText>
+          </WrapperTech>
+
+          <WrapperTech>
+            <Logo src={LogoSASS} alt="Logo SASS" />
+            <WrapperTechText>SASS</WrapperTechText>
+          </WrapperTech>
+
+          <WrapperTech>
+            <Logo src={LogoRedux} alt="Logo Redux" />
+            <WrapperTechText>Redux Toolkit</WrapperTechText>
+          </WrapperTech>
         </WrapperLogos>
         <NuevoTituloSeccion>{t("extraInfo")}</NuevoTituloSeccion>
         <InfoExtra>{t("scrumEnglish")}</InfoExtra>
